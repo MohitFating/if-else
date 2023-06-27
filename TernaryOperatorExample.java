102f43adf960d331fc015ad9b363cf854baff428
Mohit Fating <77158042+MohitFating@users.noreply.github.com>
2023-06-27 23:52:27 +0530

    Create TernaryOperatorExample.java

---
+++ TernaryOperatorExample.java
@@ -0,0 +1,7 @@
+public class TernaryOperatorExample {
+    public static void main(String[] args) {
+        int number = 10;
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println("Number is " + result);
+    }
+}
